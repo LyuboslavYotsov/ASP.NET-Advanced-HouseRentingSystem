e2931d942238da1912a77aefb577f2eff26961f2
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-02-24 11:49:44 +0200

    Added project structure layers

--- HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseRentingSystem.Data
+namespace HouseRentingSystem.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
