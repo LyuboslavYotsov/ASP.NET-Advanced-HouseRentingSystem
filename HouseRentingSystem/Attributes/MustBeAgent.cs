51609ce073735793640a077ba2f10741445330c5
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-03-02 14:55:12 +0200

    MustBeAgent attribute added

---
+++ HouseRentingSystem/Attributes/MustBeAgent.cs
@@ -0,0 +1,29 @@
+﻿using HouseRentingSystem.Controllers;
+using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HouseRentingSystem.Attributes
+{
+    public class MustBeAgent : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService != null && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
+            }
+
+        }
+    }
+}
