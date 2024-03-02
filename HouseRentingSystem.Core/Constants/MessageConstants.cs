7346f19ec746ae2508d5c0ac1642400d25b49a6b
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-03-02 13:49:10 +0200

    BecomeAgentModel created

---
+++ HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequireMessage = "Field {0} is required!";
+
+        public const string LengthMessage = "Field {0} must be between {2} and {1} characters long!";
+    }
+}
--- HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace HouseRentingSystem.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Infrastructure.Data.Constants.DataConstants;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+
+namespace HouseRentingSystem.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequireMessage)]
+        [StringLength(AgentPhoneMaxLength,
+            MinimumLength = AgentPhoneMinLength,
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
     }
 }
